480716a2396da49614045de010302595e2044004
matvisiv2 <matvisiv2@gmail.com>
2025-05-26 12:17:33 +0300

    task 04

--- js/HOMEWORK4.js
+++ js/HOMEWORK4.js
@@ -110,10 +110,51 @@ function task03 () {
     }
 }
 
-// 4. Реалізуйте функцію showUser(id), яка приймає параметром користувацьке id і повертає об’єкт, який містить значення переданої id. Також функція викидає помилку у разі якщо введено від’ємне id.
-// 	Реалізуйте функцію showUsers(ids), яка приймає параметром масив користувацьких айді ids, перевіряє з використанням функції showUser() кожен елемент масиву ids на коректність, в разі виключної ситуації виводить повідомлення про помилку. Функція showUsers(ids) повертає масив об’єктів, де значеннями ключів є коректні елементи ids.
+// 4. Реалізуйте функцію showUser(id), яка приймає параметром користувацьке id
+// і повертає об’єкт, який містить значення переданої id.
+// Також функція викидає помилку у разі якщо введено від’ємне id.
+
+// 	Реалізуйте функцію showUsers(ids), яка приймає параметром масив користувацьких айді ids,
+// перевіряє з використанням функції showUser() кожен елемент масиву ids на коректність,
+// в разі виключної ситуації виводить повідомлення про помилку.
+// Функція showUsers(ids) повертає масив об’єктів, де значеннями ключів є коректні елементи ids.
 
 // 	Приклад роботи програми:
 // showUsers([7, -12, 44, 22]);
 // Error: ID must not be negative: -12
 // [ {id: 7}, {id: 44}, {id: 22} ]
+
+function showUser (id) {
+    if (id < 0) {
+        throw Error(`ID must not be negative: ${id}`);
+    }
+    return { id };
+}
+
+function showUsers (ids) {
+    const result = [];
+
+    for (let id of ids) {
+        try {
+            const user = showUser(id);
+            result.push(user);
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
+    }
+
+    return result;
+}
+
+function task04 () {
+    const inputArray = document
+        .getElementById('idsT04')
+        .value.replace(/\s+/g, '')      // remove all spaces
+        .split(',');                    // to array
+    const resultElement = document.getElementById('resultT04');
+
+    const result = showUsers(inputArray).filter((obj) => obj != null);
+
+    resultElement.innerHTML = JSON.stringify(result);
+    console.log(`result = ${JSON.stringify(result)}`);
+}
